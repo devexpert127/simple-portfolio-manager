5bc399f2c4edc68f9b95f5ab04dd93813a9514bc
Lyappunov <lyappunov19@outlook.com>
2022-03-09 22:47:13 -0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,11 +23,15 @@ export const getAllWalletOptions = async (
   
   const optionMarkets = await getAllOptionAccounts(program);
   // Load all SPL Mint addresses of the connected walletKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+  // const resp = await program.provider.connection.getTokenAccountsByOwner(
+  //   new PublicKey('Hq3XnZMfjUvcKitYMB1abYshRZnufS1sSovzRztGXg8s'),
+  //   { mint:new PublicKey('2GzKsbGV5TwXrzAKTiCu8jq9d5KWkzuNFv2rpZPJHxvB') }
+  //   // program.provider.wallet.publicKey,
+  //   // { mint:new PublicKey('BzwRWwr1kCLJVUUM14fQthP6FJKrGpXjw3ZHTZ6PQsYa'), programId: TOKEN_PROGRAM_ID }
+  // );
   const resp = await program.provider.connection.getTokenAccountsByOwner(
-    new PublicKey('Hq3XnZMfjUvcKitYMB1abYshRZnufS1sSovzRztGXg8s'),
-    { mint:new PublicKey('2GzKsbGV5TwXrzAKTiCu8jq9d5KWkzuNFv2rpZPJHxvB') }
-    // program.provider.wallet.publicKey,
-    // { mint:new PublicKey('BzwRWwr1kCLJVUUM14fQthP6FJKrGpXjw3ZHTZ6PQsYa'), programId: TOKEN_PROGRAM_ID }
+    program.provider.wallet.publicKey,
+    { programId: TOKEN_PROGRAM_ID }
   );
 
   const tokenAccounts: Record<string, TokenAccount> = {};
